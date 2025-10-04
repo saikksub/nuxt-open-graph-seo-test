20c0d55ea61abe5503e71fbcfbed7ac53aa84288
Sai K K <saikksub@gmail.com>
2025-10-04 18:43:20 +0530

    feat: add server and map to seo meta

--- app/app.vue
+++ app/app.vue
@@ -8,14 +8,16 @@
 </template>
 
 <script setup>
+const { data: seoData } = useAsyncData('test', () => $fetch('/api/get-seo'))
+
 useSeoMeta({
-  title: 'Sai',
-  description: 'Create high-quality web applications with Nuxt, the open source framework that makes full-stack development with Vue.js intuitive.',
-  ogTitle: 'Sai',
-  ogDescription: 'Create high-quality web applications with Nuxt, the open source framework that makes full-stack development with Vue.js intuitive.',
-  ogImage: 'https://images.unsplash.com/photo-1759434225861-e834192ccdaf?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  ogImageUrl: 'https://images.unsplash.com/photo-1759434225861-e834192ccdaf?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  ogImageAlt: 'Test OG Image'
+  title: () => seoData.value?.title ?? 'AA',
+  description: () => seoData.value?.description ?? 'AA',
+  ogTitle: () => seoData.value?.ogTitle ?? 'AA',
+  ogDescription: () => seoData.value?.ogDescription ?? 'AA',
+  ogImage: () => seoData.value?.ogImage ?? 'https://images.unsplash.com/photo-1759503408354-8cfa8a13115e?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  ogImageUrl: () => seoData.value?.ogImageUrl ?? 'https://images.unsplash.com/photo-1759503408354-8cfa8a13115e?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  ogImageAlt: () => seoData.value?.ogImageAlt ?? 'AA',
 })
 
 useHead({
---
+++ server/api/get-seo.ts
@@ -0,0 +1,14 @@
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+export default defineEventHandler(async () => {
+  await delay(1000);
+  return {
+    title: 'Sai (From Server)',
+    description: 'Create high-quality web applications with Nuxt, the open source framework that makes full-stack development with Vue.js intuitive.',
+    ogTitle: 'Sai (From Server)',
+    ogDescription: 'Create high-quality web applications with Nuxt, the open source framework that makes full-stack development with Vue.js intuitive.',
+    ogImage: 'https://images.unsplash.com/photo-1759434225861-e834192ccdaf?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    ogImageUrl: 'https://images.unsplash.com/photo-1759434225861-e834192ccdaf?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    ogImageAlt: 'Test OG Image'
+  }
+})
