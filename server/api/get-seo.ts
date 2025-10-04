fbde0eae136b92ff572b4b77978c99d4cc1d1d50
Sai K K <saikksub@gmail.com>
2025-10-04 18:55:37 +0530

    refactor: change image url

--- server/api/get-seo.ts
+++ server/api/get-seo.ts
@@ -7,8 +7,8 @@ export default defineEventHandler(async () => {
     description: 'Create high-quality web applications with Nuxt, the open source framework that makes full-stack development with Vue.js intuitive.',
     ogTitle: 'Sai (From Server)',
     ogDescription: 'Create high-quality web applications with Nuxt, the open source framework that makes full-stack development with Vue.js intuitive.',
-    ogImage: 'https://images.unsplash.com/photo-1759434225861-e834192ccdaf?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    ogImageUrl: 'https://images.unsplash.com/photo-1759434225861-e834192ccdaf?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    ogImage: 'https://images.unsplash.com/photo-1759503408354-8cfa8a13115e?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    ogImageUrl: 'https://images.unsplash.com/photo-1759503408354-8cfa8a13115e?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     ogImageAlt: 'Test OG Image'
   }
 })
