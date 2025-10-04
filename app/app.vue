8c4ab395d82bdfe70eddc8eb63bd22658f9cfbfd
Sai K K <saikksub@gmail.com>
2025-10-04 18:29:02 +0530

    refactor: 100 char length desc, image

--- app/app.vue
+++ app/app.vue
@@ -10,11 +10,11 @@
 <script setup>
 useSeoMeta({
   title: 'Sai',
-  description: 'Test description',
+  description: 'Create high-quality web applications with Nuxt, the open source framework that makes full-stack development with Vue.js intuitive.',
   ogTitle: 'Sai',
-  ogDescription: 'Test description',
-  ogImage: '/images/og.png',
-  ogImageUrl: '/images/og.png',
+  ogDescription: 'Create high-quality web applications with Nuxt, the open source framework that makes full-stack development with Vue.js intuitive.',
+  ogImage: 'https://images.unsplash.com/photo-1759434225861-e834192ccdaf?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  ogImageUrl: 'https://images.unsplash.com/photo-1759434225861-e834192ccdaf?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   ogImageAlt: 'Test OG Image'
 })
 
